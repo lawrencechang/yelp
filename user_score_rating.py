1d9948db5e71058010cb138189e0942de29c87c6
Lawrence Chang <chang@badassimac.local>
2014-05-01 13:48:55 -0700

    I think this commit has the actual change to gitignore. Also slightly further updates to user_score_rating. Not complete yet.

--- user_score_rating.py
+++ user_score_rating.py
@@ -2,19 +2,35 @@
 # 1. User rating correlation
 # Get the reviews of this user
 
-import cPickle as pickle
+import cPickle as pickle;
+import time;
 
+start = time.clock();
 print 'Loading user list from users.p file.';
 users = pickle.load(open('users.p', 'rb'));
 print 'Finished loading user list.';
 
-print 'Loading user list from users.p file.';
-users = pickle.load(open('users.p', 'rb'));
-print 'Finished loading user list.';
+print 'Loading user_to_review_ids list from user_to_review_ids.p file.';
+user_to_review_ids = pickle.load(open('user_to_review_ids.p', 'rb'));
+print 'Finished loading user_to_review_ids list.';
 
+print 'Loading review list from reviews.p file.';
+reviews = pickle.load(open('reviews.p', 'rb'));
+print 'Finished loading reviews list.';
+
+elapsed = (time.clock() - start);
+print 'All the loading took '+str(elapsed)+' seconds.';
+
+count = 0;
 for user in users:
 	print users[user];
-	break;
+	users_reviews = user_to_review_ids[user]; # This is a list
+	for review_id in users_reviews:
+		print review_id;
+		print reviews[review_id];
+	if count > 5:
+		break;
+	count += 1;
 	
 	
 
