bf9ef12036401b5bee3e8449f12036b045ebe3f7
Lawrence Chang <chang@badassimac.local>
2014-05-14 13:47:09 -0700

    Script to run all scoring. Look for commented out things.

---
+++ run_scoring.py
@@ -0,0 +1,8 @@
+import os;
+
+os.system("python ./user_score_temporal.py");
+#os.system("python ./user_score_upvote.py");
+os.system("python ./user_score_rating.py");
+os.system("python ./user_score_count.py");
+
+os.system("python ./user_score.py");
